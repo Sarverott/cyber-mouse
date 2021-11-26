feaf795e35c5877c51b1bb4cd8978bb69385aa3c
Sett Sarverott <sarverott@gmail.com>
2021-11-26 05:14:42 +0100

    template of runscript based on anubis-atom-insertion.js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+if(require.main===module){// https://github.com/Sarverott/anubis-atom-insertion/commit/fa41b4086a5eaa4de03227f93c55f663adfd3aa4#diff-854754e56e77b4b86a31c4c0544d911ec91696ff5c7dadc535c9512c020ee89eR167
+  if(process.argv.length==2){
+    //
+  }else if(process.argv.length==3){
+    //
+  }else{
+    //
+  }
+}else{
+  module.exports={
+    //
+  };
+}
